548ef20b33f80b6c68237bb86106e72453d21517
18838988676@163.com <git18838988676>
2019-08-03 20:02:53 +0800

    用户注册等

---
+++ Rabbitmq_demo/src/test/java/MqTest.java
@@ -0,0 +1,40 @@
+import com.itheima.mq.Application;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class MqTest {
+
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Test
+    public void testSend(){
+        rabbitTemplate.convertAndSend("itcast","你死不死啊");
+    }
+
+
+    @Test
+    public void testSendFanout(){
+        rabbitTemplate.convertAndSend("chuanzhi","","分列模式了解一下?");
+    }
+
+
+    @Test
+    public void testSendTopic1(){
+        rabbitTemplate.convertAndSend("topictest","goods.aaa","主题模式绑定itcast");
+    }
+
+    @Test
+    public void testSendTopic2(){
+        rabbitTemplate.convertAndSend("topictest","article.content.log","主题模式绑定itheima");
+    }
+
+
+}
